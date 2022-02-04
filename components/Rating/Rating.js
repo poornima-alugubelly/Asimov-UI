af5719a93311c39b5bdb11aacde193cf430c2780
poornima-alugubelly <reddypoornima683@gmail.com>
2022-02-04 13:14:59 +0530

    refactor: improved accessibility

--- components/Rating/Rating.js
+++ components/Rating/Rating.js
@@ -1,14 +1,24 @@
-const ratings = document.querySelectorAll(".rating-dynamic");
+const ratings = document.querySelectorAll(".rating");
+const ratingStarForm = document.querySelector(".rating-star-form");
+const ratingIcons = document.querySelectorAll(".rating .rating-star-icon");
 
-ratings.forEach((rating) =>
-  rating.addEventListener("click", () => {
-    for (i = 0; i < rating.dataset.starValue; i++) {
-      ratings[i].classList.add("fas");
-      ratings[i].classList.remove("far");
+ratings.forEach((rating, id) =>
+  rating.addEventListener("change", () => {
+    for (i = 0; i <= id; i++) {
+      ratingIcons[i].classList.add("fas");
+      ratingIcons[i].classList.remove("far");
     }
-    for (i = rating.dataset.starValue; i < 5; i++) {
-      ratings[i].classList.remove("fas");
-      ratings[i].classList.add("far");
+    for (i = id + 1; i < 5; i++) {
+      ratingIcons[i].classList.remove("fas");
+      ratingIcons[i].classList.add("far");
     }
   })
 );
+
+ratingStarForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  ratings.forEach((rating) => {
+    const ratingInput = rating.querySelector('input[type="radio"]');
+    if (ratingInput.checked) console.log(ratingInput.value);
+  });
+});
