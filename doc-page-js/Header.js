94dada38ba6f1368c79b89ca6d980e42dc8dc52e
poornima-alugubelly <reddypoornima683@gmail.com>
2022-02-07 17:40:42 +0530

    Chore: Fixed carbon snippets

--- doc-page-js/Header.js
+++ doc-page-js/Header.js
@@ -1,4 +1,3 @@
-const btnDropdownAll = document.querySelectorAll(".btn-dropdown");
 const btnDismiss = document.querySelector(".btn-dismiss");
 const drawerToggels = document.querySelectorAll(".drawer-toggle");
 
@@ -13,20 +12,6 @@ function openDrawer() {
 
 	currDrawer.classList.add("active");
 }
-
-function dropDownHandler() {
-	const dropdownIcon = this.querySelector(".icon-dropdown");
-
-	const dropdown = this.closest(".dropdown-container").querySelector(
-		".drop-down"
-	);
-	dropdownIcon.classList.toggle("fa-chevron-down");
-	dropdownIcon.classList.toggle("fa-chevron-up");
-	dropdown.classList.toggle("btn-drop-down-active");
-}
-btnDropdownAll.forEach((btndropdown) =>
-	btndropdown.addEventListener("click", dropDownHandler)
-);
 
 drawerToggels.forEach((toggle) => toggle.addEventListener("click", openDrawer));
 btnDismiss.addEventListener("click", closeDrawer);
