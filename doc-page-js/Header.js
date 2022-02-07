6453e963f28d133480efaae2cd6a129c625cc101
poornima-alugubelly <reddypoornima683@gmail.com>
2022-02-07 12:38:21 +0530

    Fix : final fixes

--- doc-page-js/Header.js
+++ doc-page-js/Header.js
@@ -4,11 +4,13 @@ const drawerToggels = document.querySelectorAll(".drawer-toggle");
 
 function closeDrawer() {
 	const currDrawer = this.closest(".drawer");
+
 	currDrawer.classList.remove("active");
 }
 
 function openDrawer() {
 	const currDrawer = document.querySelector(this.dataset.drawerTarget);
+
 	currDrawer.classList.add("active");
 }
 
