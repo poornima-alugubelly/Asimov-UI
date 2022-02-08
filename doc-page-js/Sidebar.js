81ec1d2826ebde2dddb8e66ce97b9ad27f209cef
poornima-alugubelly <reddypoornima683@gmail.com>
2022-02-08 13:28:05 +0530

    fix: fixed code snippets

--- doc-page-js/Sidebar.js
+++ doc-page-js/Sidebar.js
@@ -1,9 +1,10 @@
 function SideBarHandler() {
 	sideNavLinks.classList.toggle("side-nav-active");
+
 	dropdownIcon.classList.toggle("fa-chevron-down");
 	dropdownIcon.classList.toggle("fa-chevron-up");
 }
-const sideNavLinks = document.querySelector(".side-nav-links");
+const sideNavLinks = document.querySelector(".side-nav");
 const navBarBtn = document.querySelector(".nav-side-bar-btn");
 const dropdownIcon = navBarBtn.querySelector(".side-bar-icon");
 navBarBtn.addEventListener("click", SideBarHandler);
